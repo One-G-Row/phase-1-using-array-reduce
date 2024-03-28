2ab214b804afd79e99ae6d4ab8e1c93873a0f9e4
student <kmac.kimaru93@gmail.com>
2024-03-28 19:54:44 +0300

    adding array reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+//declare totalBatteries globally
+let totalBatteries = 0;
+function reducer() {
+  totalBatteries = batteryBatches.reduce((total, batch) => {
+    return (total += batch);
+  });
+}
+console.log(reducer());
